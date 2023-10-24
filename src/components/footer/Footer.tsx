060d7aaefec5cfd766e0bc95dd54ee993f008566
Adriana Gioielli <adrianalves.cp@gmail.com>
2023-10-24 11:57:15 -0300

    Blog pessoal completo
    ainda faltam ajustes, cosméticos principalmente.

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,36 @@
+import React, { useContext } from 'react'
+import { FacebookLogo, InstagramLogo, LinkedinLogo } from '@phosphor-icons/react'
+import { AuthContext } from '../../contexts/AuthContext'
+
+function Footer() {
+  
+  const { usuario, handleLogout } = useContext(AuthContext)
+  let footerComponent
+  let data = new Date().getFullYear()
+
+  if(usuario.token !== '') {
+    footerComponent = (
+      <>
+        <div className="flex justify-center bg-indigo-900 text-white">
+          <div className="container flex flex-col items-center py-4">
+            <p className='text-xl font-bold'>Blog pessoal Generation | Copyright: {data}</p>
+            <p className='text-lg'>Acesse nossas redes sociais</p>
+            <div className='flex gap-2'>
+              <LinkedinLogo size={36} weight='bold' />
+              <InstagramLogo size={36} weight='bold' />
+              <FacebookLogo size={36} weight='bold' />
+            </div>
+          </div>
+        </div>
+      </>
+    )
+  }
+
+  return (
+    <>
+        {footerComponent}
+      </>
+  )
+}
+
+export default Footer
